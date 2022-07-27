e91e0cff42ba2ed34ea14e08ec2f7435bb71ebd7
Sophie Martina Margarete Brunner <95913912+sophiebrunner@users.noreply.github.com>
2022-07-27 19:38:44 +0200

    feat: add change-method to synchronize done-status

--- script.js
+++ script.js
@@ -17,9 +17,32 @@ Vue.createApp({
       text: "",
     };
   },
+  created() {
+    this.getData();
+  },
   methods: {
+    saveData() {
+      localStorage.setItem("toDos", JSON.stringify(this.toDos));
+    },
+    getData() {
+      this.toDos = JSON.parse(localStorage.getItem("toDos"));
+    },
     addToDo() {
-      this.toDos.push(this.text);
+      this.toDos.push({
+        description: this.text,
+        done: false,
+        id: this.toDos.length + 1,
+      });
+      this.saveData();
+    },
+    onCheckboxChange(id, event) {
+      const isDone = event.target.checked;
+      const currentToDo = this.toDos.find((toDo) => toDo.id === id);
+      console.log(currentToDo);
+      console.log(id);
+      currentToDo.done = isDone;
     },
   },
 }).mount("#app");
+
+// Wenn To Dos löschen möglich: größte id rausfiltern und dort +1
